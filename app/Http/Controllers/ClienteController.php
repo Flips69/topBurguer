9d56871d745435f06909664abd7086a3cdd2ccad
Flips69 <aion.lipe@gmail.com>
2024-04-04 15:42:12 -0300

    04/04, controller, request, model e migration do Cliente

---
+++ app/Http/Controllers/ClienteController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cliente;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class ClienteController extends Controller
+{
+    public function cadastrarCliente(Request $request){
+        $clientes = Cliente::create([
+            'nome' => $request->nome,
+            'telefone' => $request->telefone,
+            'endereco' => $request->endereco,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            "status" => true,
+            "message" => "Cliente foi cadastrado com sucesso.",
+            "data" => $clientes
+        ], 200);
+    }
+}
